ffa23087a334980129fbf3a1933a7a8ac4f10b05
PhilULSCI <60559527+PhilULSCI@users.noreply.github.com>
2026-08-15 22:00:21 -0700

    Make the RPG-7 a pure anti-tank weapon

--- lua/entities/m9k_gdcwa_rpg_heat.lua
+++ lua/entities/m9k_gdcwa_rpg_heat.lua
@@ -58,7 +58,7 @@ if SERVER then
                 self:Remove()
                 return
             end
-            util.BlastDamage( self, self.Owner, tr.HitPos, 600, 150 )
+            util.BlastDamage( self, self.Owner, tr.HitPos, 100, 2000 )
             local effectdata = EffectData()
             effectdata:SetOrigin( tr.HitPos ) -- Where is hits
             effectdata:SetNormal( tr.HitNormal ) -- Direction of particles
